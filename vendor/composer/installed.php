77580b7bdd7f320549d2abf322f80d4e022b3489
Muhammed Salama <32281997+MohammedSalama@users.noreply.github.com>
2023-11-02 07:50:22 +0200

    Updates

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wdd/slug',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'd15483eed3a47871cb603eb88e172bb6b01def3c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'wdd/slug' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'd15483eed3a47871cb603eb88e172bb6b01def3c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
